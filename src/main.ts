56dddbd5791b592e462ef98791611c20dc756f4c
junghojang <cookyman@gmail.com>
2024-11-21 21:42:18 +0900

    - activate swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Swagger 설정
+  const config = new DocumentBuilder()
+    .setTitle('IAM Server API')
+    .setDescription('OAuth 인증 서버 API 문서')
+    .setVersion('1.0')
+    .addBearerAuth() // JWT 토큰 인증 추가
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document); // Swagger 경로 설정
+
+  await app.listen(3000);
 }
 bootstrap();
